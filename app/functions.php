5002194d8dfa648429ff2c196598a0eaf6da04e8
chang <changpeng94@mail.com>
2023-12-20 17:54:30 +0800

    chang function

--- app/functions.php
+++ app/functions.php
@@ -1,4 +1,19 @@
 <?php
+
 /**
  * Here is your custom functions.
  */
+
+/**
+ * @return string
+ */
+function  uuid(): string
+{
+  $chars = md5(uniqid(mt_rand(), true));
+  $uuid = substr($chars, 0, 8) . '-'
+    . substr($chars, 8, 4) . '-'
+    . substr($chars, 12, 4) . '-'
+    . substr($chars, 16, 4) . '-'
+    . substr($chars, 20, 12);
+  return $uuid;
+}
--- config/server.php
+++ config/server.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of webman.
  *
@@ -13,19 +14,19 @@
  */
 
 return [
-    'listen' => 'http://0.0.0.0:8787',
-    'transport' => 'tcp',
-    'context' => [],
-    'name' => 'webman',
-    'count' => cpu_count() * 4,
-    'user' => '',
-    'group' => '',
-    'reusePort' => false,
-    'event_loop' => '',
-    'stop_timeout' => 2,
-    'pid_file' => runtime_path() . '/webman.pid',
-    'status_file' => runtime_path() . '/webman.status',
-    'stdout_file' => runtime_path() . '/logs/stdout.log',
-    'log_file' => runtime_path() . '/logs/workerman.log',
-    'max_package_size' => 10 * 1024 * 1024
+  'listen' => 'http://0.0.0.0:8787',
+  'transport' => 'tcp',
+  'context' => [],
+  'name' => 'webman',
+  'count' => cpu_count(),
+  'user' => '',
+  'group' => '',
+  'reusePort' => false,
+  'event_loop' => '',
+  'stop_timeout' => 2,
+  'pid_file' => runtime_path() . '/webman.pid',
+  'status_file' => runtime_path() . '/webman.status',
+  'stdout_file' => runtime_path() . '/logs/stdout.log',
+  'log_file' => runtime_path() . '/logs/workerman.log',
+  'max_package_size' => 10 * 1024 * 1024
 ];
